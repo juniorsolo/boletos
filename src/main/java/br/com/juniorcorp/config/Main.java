20087f7a0c93290b21255c848dd6b1c1a530b9db
juniorsolo <junior.solo@hotmail.com>
2018-10-27 10:42:14 -0200

    Configurações pre-producao

--- src/main/java/br/com/juniorcorp/config/Main.java
+++ src/main/java/br/com/juniorcorp/config/Main.java
@@ -3,6 +3,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Main {
 //	public static void main(String[] args) {
 //		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//		System.out.println(encoder.encode("123"));
+//		System.out.println(encoder.encode("20moretto18"));
 //	}
 }  
